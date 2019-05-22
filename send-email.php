6c75ceeba67ef9d1f3cdca7be4031a068a583b19
Iris Bromley <iris.j.bromley@gmail.com>
2019-05-22 13:29:25 +0200

    modal content change

--- send-email.php
+++ send-email.php
@@ -5,8 +5,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $size = $_POST['size'];
-$message = $_POST['message'];
-$body="_________________________________ \n Anfrage für 3STG: \n Absender: $anrede $titel $name \n E-Mail: $email \n Telefonnummer: $phone \n Size: $size \n Nachricht: $message ";
+$body="_________________________________ \n Anfrage für 3STG: \n Absender: $anrede $titel $name \n E-Mail: $email \n Telefonnummer: $phone \n Size: $size";
 $to = "iris.pfeffer@gmx.at";
 $subject = "Anfrage für 3STG";
 if($name != "" && $email != ""){  
